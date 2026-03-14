da6b4b765268f58413efdb20de67757b1e0fa9f6
Pohan19 <bruce.l0919@gmail.com>
2026-03-14 15:43:38 +0800

    Create MarketMonitor component for watchlist and alerts

---
+++ src/pages/MarketMonitor.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const MarketMonitor = () => {
+    return (
+        <div>
+            <h1>Market Monitor</h1>
+            <h2>Watchlist</h2>
+            {/* Add Watchlist Component */}
+            <h2>Alerts</h2>
+            {/* Add Alerts Component */}
+        </div>
+    );
+};
+
+export default MarketMonitor;
